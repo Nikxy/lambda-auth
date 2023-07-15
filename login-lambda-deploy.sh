e67dfd5676e072bf629cc3a0cf6270eee043052d
Diana <nikxy3@gmail.com>
2023-07-15 23:45:20 +0300

    create deployment scripts

---
+++ login-lambda-deploy.sh
@@ -0,0 +1,17 @@
+LOG_FILE=./login-lambda-deploy.log
+LAMBDA_NAME=lambda-login
+
+echo 'Starting demployment...'
+if [ -e $LOG_FILE ]
+then
+    echo 'Clearing log file'
+    rm $LOG_FILE
+fi
+echo 'Creating zip archive...'
+cd $LAMBDA_NAME
+zip -r $LAMBDA_NAME.zip * &> /dev/null
+echo 'Uploading...'
+aws lambda --function-name update-function-code nikxy-auth-login --zip-file fileb://$LAMBDA_NAME.zip  &> ../$LOG_FILE
+echo 'Removing zip archive...'
+rm $LAMBDA_NAME.zip
+echo 'Done.'
---
+++ status-lambda-deploy.sh
@@ -0,0 +1,17 @@
+LOG_FILE=./status-lambda-deploy.log
+LAMBDA_NAME=lambda-status
+
+echo 'Starting demployment...'
+if [ -e $LOG_FILE ]
+then
+    echo 'Clearing log file'
+    rm $LOG_FILE
+fi
+echo 'Creating zip archive...'
+cd $LAMBDA_NAME
+zip -r ../$LAMBDA_NAME.zip * &> /dev/null
+echo 'Uploading...'
+aws lambda --function-name update-function-code nikxy-auth-status --zip-file fileb://../$LAMBDA_NAME.zip  &> ../$LOG_FILE
+echo 'Removing zip archive...'
+rm ../$LAMBDA_NAME.zip
+echo 'Done.'

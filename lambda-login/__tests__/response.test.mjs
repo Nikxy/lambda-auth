61068ca974ccb95be3bb6f33954d19ea0245c209
Nikxy <nikxy3@gmail.com>
2023-07-23 00:46:26 +0300

    refractoring

--- lambda-login/__tests__/response.test.mjs
+++ lambda-login/__tests__/response.test.mjs
@@ -1,43 +1,54 @@
 import response from "../utils/response";
 
-describe('util/response', () => {
+const testData = {
+  test: "test",
+};
+const errorData = {
+  error: "test",
+};
 
-    test("response", async () => {
-    const data = { test: "test" };
-    const res = response.generate(200, data);
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual(data);
-    }
-);
-test("responseEmptyOK", async () => {
-    const res = response.generateEmptyOK();
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toBeNull();
-    }
-);
-test("responseOK", async () => {
-    const data = { test: "test" };
-    const res = response.generateOK(data);
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual(JSON.stringify(data));
-    }
-);
-test("responseBadRequest", async () => {
-    const res = response.generateBadRequest();
-    expect(res.statusCode).toBe(400);
-    expect(res.body).toBeNull();
-    }
-);
-test("responseBadRequest", async () => {
-    const res = response.generateBadRequest("test");
-    expect(res.statusCode).toBe(400);
-    expect(res.body).toEqual({ error: "test" });
-    }
-);
-test("responseServerError", async () => {
-    const res = response.generateServerError("test");
-    expect(res.statusCode).toBe(500);
-    expect(res.body).toEqual({ error: "test" });
-    }
-);
+describe("util class response", () => {
+  describe("generate", () => {
+    it("should respond with specified status & data", async () => {
+      const res = response.generate(200, testData);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual(testData);
+    });
+  });
+  describe("generateOK", () => {
+    it("should respond ok without data", async () => {
+      const res = response.generateOK();
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toBeNull();
+    });
+    it("should respond ok with data", async () => {
+      const res = response.generateOK(testData);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual(testData);
+    });
+  });
+  describe("generateBadRequest", () => {
+    it("should respond 400 without data", async () => {
+      const res = response.generateBadRequest();
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toBeNull();
+    });
+    it("should respond 400 with data", async () => {
+      const res = response.generateBadRequest(errorData.error);
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toEqual(errorData);
+    });
+  });
+  describe("generateServerError", () => {
+    it("should respond 500 without data", async () => {
+        const res = response.generateServerError();
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toBeNull();
+      });
+    it("should respond 500 with data", async () => {
+      const res = response.generateServerError("test");
+      expect(res.statusCode).toBe(500);
+      expect(res.body).toEqual({ error: "test" });
+    });
+  });
 });

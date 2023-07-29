2ed0125a8b8a8e5d28ca29d8748a235773738462
Nikxy <nikxy3@gmail.com>
2023-07-30 00:06:24 +0300

    add tests

--- lambda-login/__tests__/index.test.mjs
+++ lambda-login/__tests__/index.test.mjs
@@ -1,1 +1,41 @@
 import { handler } from '../index.mjs';
+
+describe('lambda-login', () => {
+    test('body should be valid JSON', async () => {
+        const event = {
+            body: "adsasd",
+        };
+        const response = await handler(event);
+        expect(response.statusCode).toBe(400);
+    });
+    test('body should contain domain,username and password ', async () => {
+        const event = {
+            body: JSON.stringify({
+                domain: "test",
+            }),
+        };
+        const response = await handler(event);
+        expect(response.statusCode).toBe(400);
+    });
+    test('body should contain domain,username and password ', async () => {
+        const event = {
+            body: JSON.stringify({
+                domain: "test",
+                password: "test",
+            }),
+        };
+        const response = await handler(event);
+        expect(response.statusCode).toBe(400);
+    });
+    test('body should contain domain,username and password ', async () => {
+        const event = {
+            body: JSON.stringify({
+                domain: "test",
+                username: "test",
+                password: "test",
+            }),
+        };
+        const response = await handler(event);
+        expect(response.statusCode).toBe(400);
+    });
+});

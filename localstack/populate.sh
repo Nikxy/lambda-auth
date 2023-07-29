3d0bee2405ebcd00b39cb2ae1c685b335469e537
Nikxy <nikxy3@gmail.com>
2023-07-30 00:05:14 +0300

    add doc_ to reserved keyword

--- localstack/populate.sh
+++ localstack/populate.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 userData=$(<localstack/test-user.json)
+secretData=$(<localstack/test-secret.json)
 
 docker exec localstack_main awslocal dynamodb create-table \
     --table-name nikxy-auth \
     --attribute-definitions \
-        AttributeName=Id,AttributeType=S \
-        AttributeName=Type,AttributeType=S \
+        AttributeName=id,AttributeType=S \
+        AttributeName=doc_type,AttributeType=S \
     --key-schema \
-        AttributeName=Id,KeyType=HASH \
-        AttributeName=Type,KeyType=RANGE \
+        AttributeName=id,KeyType=HASH \
+        AttributeName=doc_type,KeyType=RANGE \
     --provisioned-throughput \
         ReadCapacityUnits=5,WriteCapacityUnits=5 \
     --table-class STANDARD
@@ -16,3 +17,7 @@ docker exec localstack_main awslocal dynamodb create-table \
 docker exec localstack_main awslocal dynamodb put-item \
     --table-name nikxy-auth \
     --item="$userData"
+
+docker exec localstack_main awslocal secretsmanager create-secret \
+    --name "nikxy/auth/jwtsecrets" \
+    --secret-string "$secretData"

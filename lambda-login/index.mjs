cfedb47cde84ee2d8f6f14874ce61280fa59efe0
Nikxy <nikxy3@gmail.com>
2023-07-30 00:03:12 +0300

    create login-lambda with dynamodb

--- lambda-login/index.mjs
+++ lambda-login/index.mjs
@@ -1,74 +1,105 @@
 import response from "./utils/response.js";
-/*
+import AWS from "aws-sdk";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import createJwtToken from "./utils/createJwtToken.js";
+import { v4 as uuidv4 } from "uuid";
 import getSecret from "./utils/getSecret.js";
-import dbRepository from "./utils/dbRepository.js";
-*/
 
+// Check if all env variables are set
+if (process.env.DB_TABLE == undefined) {
+  console.error("env DB_TABLE not set");
+  process.exit(1);
+}
+if (process.env.JWT_SECRET_KEY == undefined) {
+  console.error("env JWT_SECRET_KEY not set");
+  process.exit(1);
+}
+
+// Configure AWS DynamoDB
+const dbConfig = { region: "eu-central-1" };
+// Set endpoint if provided
+if (process.env.DB_ENDPOINT) dbConfig.endpoint = process.env.DB_ENDPOINT;
+const docClient = new AWS.DynamoDB.DocumentClient(dbConfig);
+
+// Create Lambda Handler
 export const handler = async (event) => {
-
-  return response.generateOK();
-    // Init data from request, check if valid
-  /*let data;
+  // Init data from request, check if valid
+  let data;
   try {
     data = initData(event.body);
   } catch (e) {
-    return responseBadRequest(e.message);
+    return response.BadRequest(e.message);
   }
 
-  if(data.create == "yeah") {
-    try {
-      const hashedPassword = bcrypt.hashSync(data.password, 10);
-      await dbRepository.putUser(data.username, data.domain, hashedPassword);
-    } catch (e) {
-      console.error("Can't create user: " + e);
-      return responseServerError("Server error");
-    }
-    return responseEmptyOK();
-  }*/
-/*
-    // Get user from db
-  const resUser = await postpresql.query(
-    "SELECT * FROM users WHERE username = $1 AND domain = $2",
-    [data.username, data.domain]
-  );
-// Check if user exists
-  if (resUser.rows.length === 0)
-    return responseBadRequest("Invalid username or password");
+  // Init JWT secret
+  let jwtSecret;
+  try {
+    jwtSecret = await getSecret(process.env.JWT_SECRET_KEY);
+  } catch (e) {
+    return response.ServerError(e.message);
+  }
+  // Check if domain secret is set
+  if (!jwtSecret[data.domain])
+    return response.ServerError(
+      "Can't get jwt secret for domain: " + data.domain
+    );
 
-  const user = resUser.rows[0];
+  // Get user from DynamoDB
+  let results;
+  try {
+    const params = {
+      TableName: process.env.DB_TABLE,
 
+      ExpressionAttributeValues: {
+        ":typeVal": "user",
+        ":name": data.username,
+        ":domainVal": data.domain,
+      },
+      FilterExpression:
+        "doc_type = :typeVal and username = :name and doc_domain = :domainVal",
+    };
+    results = await docClient.scan(params).promise();
+  } catch (e) {
+    return response.ServerError(e.message);
+  }
+  // Check if user exists
+  if (results.Items.length < 1)
+    return response.BadRequest("Invalid username or password [1]");
+
+  const user = results.Items[0];
+  // Check if password is correct
   if (!bcrypt.compareSync(data.password, user.password))
-    return responseBadRequest("Invalid username or password");
+    return response.BadRequest("Invalid username or password [2]");
 
-  const jwtSecret = await getSecret(process.env.JWT_SECRET_KEY);
-  if (!jwtSecret[data.domain]) {
-    console.error("Can't get jwt secret for domain: " + data.domain);
-    return responseBadRequest("Server error");
+  // Generate refresh token
+  const refreshToken = uuidv4();
+  const dateNow = Date.now();
+  const params = {
+    TableName: process.env.DB_TABLE,
+    Item: {
+      id: refreshToken,
+      doc_type: "token",
+      doc_domain: data.domain,
+      created: dateNow,
+      valid_until: dateNow + 604800, // 7 days
+    },
+  };
+  try {
+    await docClient.put(params).promise();
+  } catch (e) {
+    return response.ServerError("Can't put refresh token: " + e.message);
   }
 
-  const tokens = await postpresql.query(
-    "SELECT uuid_generate_v4() as main,uuid_generate_v4() as refresh"
-  );
+  const tokenData = {
+    username: user.username,
+    domain: data.domain,
+    refresh_token: refreshToken,
+  };
+  const token = jwt.sign(tokenData, jwtSecret[data.domain], {
+    expiresIn: "60s",
+  });
 
-  const refreshToken = tokens.rows[0].refresh;
-  const mainToken = tokens.rows[0].main;
-  await postpresql.query(
-    "INSERT INTO tokens (id,refresh_token,user_id) VALUES ($1, $2, $3)",
-    [mainToken, refreshToken, user.id]
-  );
-
-  const token = createJwtToken(
-    data.domain,
-    user,
-    refreshToken,
-    jwtSecret[data.domain]
-  );
-  const jsonData = JSON.stringify({ token: token });
-  return { statusCode, jsonData, headers };
-  */
-  return responseBadRequest("Not implemented yet");
+  return response.OK({ token: token });
 };
 
 function initData(body) {
@@ -76,7 +107,7 @@ function initData(body) {
   try {
     data = JSON.parse(body);
   } catch (e) {
-    throw new Error("Please provide valid JSON body");
+    throw new Error("Please provide a valid JSON body");
   }
 
   if (!data.username || !data.password || !data.domain)

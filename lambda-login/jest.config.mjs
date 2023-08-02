5be5440e699c7274f2ef362f09f62fa4981cd67c
Nikxy <nikxy3@gmail.com>
2023-08-02 10:57:14 +0300

    try jest

--- lambda-login/jest.config.mjs
+++ lambda-login/jest.config.mjs
@@ -5,5 +5,7 @@ export default async () => {
     testEnvironment: "node",
     testMatch: ["**/*.test.mjs"],
     setupFiles: ["<rootDir>/__tests__/jest.setup.mjs"],
+    coveragePathIgnorePatterns: [ "/node_modules/" ],
+    moduleDirectories: ["node_modules", "./"]
   };
 };

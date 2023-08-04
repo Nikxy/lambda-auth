372809dec8814f2c6339c946ed94bbde6fc21c7a
Nikxy <nikxy3@gmail.com>
2023-08-04 11:55:08 +0300

    fix sam invoke parameters

--- test-login.sh
+++ test-login.sh
@@ -1,1 +1,4 @@
-sam local invoke LoginFunction -e events/login.json --docker-network ssl-proxy
+sam local invoke LoginFunction -e sam-events/login-right.json \
+--docker-network ssl-proxy \
+--container-host 172.17.0.1 \
+--container-host-interface 0.0.0.0

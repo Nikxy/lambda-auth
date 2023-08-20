04679338764d7c905281abf860e072cf73f92cd0
Nikxy <nikxy3@gmail.com>
2023-08-20 15:30:09 +0300

    rename folder

--- lambda-login/test/integration/tests.js
+++
@@ -1,7 +0,0 @@
-before(async () => {
-	process.env.REGION = "eu-central-1";
-    process.env.DB_TABLE = "nikxy-auth";
-	process.env.DB_ENDPOINT = "http://localstack.dev.callandorit.net";
-	process.env.SECRETS_ENDPOINT = "http://localstack.dev.callandorit.net";
-	process.env.JWT_SECRET_KEY = "nikxy/auth/jwtsecrets";
-});

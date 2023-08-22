df917e57a9c94ac6d86b52c079149f48b63d478d
Nikxy <nikxy3@gmail.com>
2023-08-22 11:40:07 +0300

    shorten variable

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ if (process.env.DB_TABLE == undefined) {
   console.error("env DB_TABLE not set");
   process.exit(1);
 }
-if (process.env.JWT_SECRET_KEY == undefined) {
+if (process.env.JWT_SECRET == undefined) {
   console.error("env JWT_SECRET_KEY not set");
   process.exit(1);
 }

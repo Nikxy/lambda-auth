53051872a176603d4e260a4ee0f6905f94dac6ed
Nikxy <nikxy3@gmail.com>
2023-08-22 11:33:23 +0300

    add template parameter

--- test-login.sh
+++ test-login.sh
@@ -1,4 +1,4 @@
-sam local invoke LoginFunction -e sam-events/login-right.json \
+sam local invoke LoginFunction -t test-template.yaml -e sam-events/empty-body.json \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \
 --container-host-interface 0.0.0.0

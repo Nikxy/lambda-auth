6e27d4c228dfe1a1f4dee869ccd32fc92da2f0ee
Nikxy <nikxy3@gmail.com>
2023-08-23 13:57:47 +0300

    add event argument

--- test-login.sh
+++ test-login.sh
@@ -1,4 +1,11 @@
-sam local invoke LoginFunction -t test-template.yaml -e sam-events/empty-body.json \
+if [ $# -eq 0 ]
+  then
+    echo "No event name supplied"
+    exit 1
+fi
+echo 'Running LoginFunction with event '$1
+
+sam local invoke LoginFunction -t test-template.yaml -e sam-events/$1.json \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \
 --container-host-interface 0.0.0.0

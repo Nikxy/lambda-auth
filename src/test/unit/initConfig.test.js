ae9b401cd02c5c08e53e1e9796d21ee80d863dac
Nikxy <nikxy3@gmail.com>
2023-08-23 15:56:18 +0300

    change checkLock to initConfig unit test

--- src/test/unit/initConfig.test.js
+++ src/test/unit/initConfig.test.js
@@ -1,10 +1,10 @@
 import assert from "assert";
-import checkLocal from "#utils/checkLocal.js";
+import initConfig from "#utils/initConfig.js";
 
 const testUrl = "http://test";
 const OLD_ENV = process.env;
 
-describe("util function checkLocal", () => {
+describe("[util] function checkLocal", () => {
 	before(() => {
 		process.env = { ...OLD_ENV }; // Make a copy
 	});
@@ -12,17 +12,15 @@ describe("util function checkLocal", () => {
 		process.env = OLD_ENV; // Restore old environment
 	});
 	it("shouldn't change config if local endpoint wasn't provided", async () => {
-		const config = {};
 
 		if (process.env.LOCAL_ENDPOINT) delete process.env.LOCAL_ENDPOINT;
 
-		checkLocal(config);
+		const config = initConfig();
 		assert.equal(config.endpoint, undefined);
 	});
 	it("should add local endpoint to config if provided", async () => {
-		const config = {};
 		process.env.LOCAL_ENDPOINT = testUrl;
-		checkLocal(config);
+		const config = initConfig();
 
 		assert.equal(config.endpoint, testUrl);
 	});

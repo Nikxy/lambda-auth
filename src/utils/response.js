3044bec55d0761d0b086e52449816141c7e41633
Nikxy <nikxy3@gmail.com>
2023-08-23 07:36:15 +0300

    change error to message to use same format as AWS

--- src/utils/response.js
+++ src/utils/response.js
@@ -2,9 +2,9 @@ class Response {
   OK = (body = null) =>
     this.generate(200, body);
   BadRequest = (msg = null) =>
-    this.generate(400, msg ? { error: msg } : null);
+		this.generate(400, msg ? { message: msg } : null);
   ServerError = (msg = null) =>
-    this.generate(500, msg ? { error: msg } : null);
+		this.generate(500, msg ? { message: msg } : null);
 
   generate(statusCode, body) {
     const headers = { "Content-Type": "application/json" };

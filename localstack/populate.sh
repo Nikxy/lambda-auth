9636e4346aec45073ca5840333c76724b130bc55
Nikxy <nikxy3@gmail.com>
2023-08-23 13:58:03 +0300

    change to compose localstack

--- localstack/populate.sh
+++ localstack/populate.sh
@@ -2,7 +2,7 @@
 userData=$(<localstack/test-user.json)
 secretData=$(<localstack/test-secret.json)
 
-docker exec localstack_main awslocal dynamodb create-table \
+docker exec callandor-localstack awslocal dynamodb create-table \
     --table-name nikxy-auth \
     --attribute-definitions \
         AttributeName=id,AttributeType=S \
@@ -14,10 +14,10 @@ docker exec localstack_main awslocal dynamodb create-table \
         ReadCapacityUnits=5,WriteCapacityUnits=5 \
     --table-class STANDARD
 
-docker exec localstack_main awslocal dynamodb put-item \
+docker exec callandor-localstack awslocal dynamodb put-item \
     --table-name nikxy-auth \
     --item="$userData"
 
-docker exec localstack_main awslocal secretsmanager create-secret \
+docker exec callandor-localstack awslocal secretsmanager create-secret \
     --name "nikxy/auth/jwtsecrets" \
     --secret-string "$secretData"

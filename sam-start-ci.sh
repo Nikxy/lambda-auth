fbaac879c58e847628c8349238ac471b28813006
Nikxy <nikxy3@gmail.com>
2023-08-23 19:36:39 +0300

    fix sam start script

--- sam-start-ci.sh
+++ sam-start-ci.sh
@@ -1,5 +1,5 @@
 venv/bin/sam local start-api -t test-template.yaml \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \
---container-host-interface 0.0.0.0
+--container-host-interface 0.0.0.0 \
 -v /home/diana/dev/jenkins_workspace/auth.nikxy.dev-login/src

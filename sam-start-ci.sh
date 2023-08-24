dc8df2a3bf3f0d819ecd976503bb79d06a8116c2
Nikxy <nikxy3@gmail.com>
2023-08-24 09:08:14 +0300

    remove warm-containers

--- sam-start-ci.sh
+++ sam-start-ci.sh
@@ -2,5 +2,4 @@ venv/bin/sam local start-api -t test-template.yaml \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \
 --container-host-interface 0.0.0.0 \
--v /home/diana/dev/jenkins_workspace/auth.nikxy.dev \
---warm-containers LAZY
+-v /home/diana/dev/jenkins_workspace/auth.nikxy.dev

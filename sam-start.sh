a22e000242935df120cd7792ab15a8551b1b479c
Nikxy <nikxy3@gmail.com>
2023-08-27 10:46:22 +0300

    add host for local development

--- sam-start.sh
+++ sam-start.sh
@@ -1,4 +1,5 @@
 sam local start-api -t test-template.yaml \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \
---container-host-interface 0.0.0.0
+--container-host-interface 0.0.0.0 \
+--host 0.0.0.0

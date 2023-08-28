076a21d1b1f49b1a5f4ccf1fde8766198ad17dbb
Nikxy <nikxy3@gmail.com>
2023-08-28 17:01:04 +0300

    create status route

--- src/routes/status.js
+++ src/routes/status.js
@@ -1,33 +1,63 @@
 import response from "#utils/response.js";
-import jwt from "jsonwebtoken";
 import getSecret from "#utils/getSecret.js";
+import repository from "#utils/authRepository.js";
+import jwt from "#utils/jwt.js";
 
 export default async function (event) {
-    if(event.headers["Authorization"] == undefined)
-        return response.BadRequest("No authorization token provided");
-    // Check if token is valid & not expired
-    const token = event.headers["Authorization"];
-    const jwttoken = jwt.decode(token);
-    if(jwttoken == null)
-        return returnInvalidToken();
-    
-    const secret = await getSecret(process.env.JWT_SECRET);
+	// Init data from request
+	let tokenData;
+	try {
+		tokenData = initData(event.headers);
+	} catch (e) {
+		return response.BadRequest(e.message);
+	}
 
-    if(secret[jwttoken.domain] == undefined)
-        return returnInvalidToken();
+	// LOAD JWT SECRET
+	let secrets;
+	try {
+		secrets = await getSecret(process.env.JWT_SECRET);
+	} catch (error) {
+		console.error("Can't get secret:", error);
+		return response.ServerError();
+	}
 
-    try {
-        jwt.verify(token, secret[jwttoken.domain]);
-    } catch (e) {
-        if(e.name == "TokenExpiredError")
-            return response.Generate(401,"Token Expired");
-        else
-            return returnInvalidToken();
-    }
+	// CHECK IF DOMAIN SECRET IS SET
+	const jwtSecret = secrets[tokenData.domain];
+	if (!jwtSecret) return response.BadRequest("Invalid domain");
 
-    return response.OK({status: "OK"});
+	// VERIFY JWT
+	const jwtStatus = jwt.validateJWT(jwtSecret, tokenData.jwt);
+	if (!jwtStatus.valid) return response.BadRequest("Invalid token");
+
+	const remainingSeconds = tokenData.exp - Math.floor(Date.now() / 1000);
+	const remainingMinutes = Math.floor(remainingSeconds / 60);
+
+	let remainingTime =
+		remainingMinutes < 1 && remainingSeconds > -1
+			? Math.abs(remainingSeconds) + " seconds"
+			: Math.abs(remainingMinutes) + " minutes";
+    let expired = remainingSeconds > 0 ? "Active for " : "Expired ";
+
+	return response.OK({ ...jwtStatus, expiryString: expired+remainingTime });
 }
 
-function returnInvalidToken(num = ""){
-    return response.Generate(401,"Invalid token."+num);
+function initData(headers) {
+	// CHECK IF AUTH HEADER IS PROVIDED
+	if (!headers["Authorization"])
+		throw new Error("Please provide a jwt authorization token");
+
+	// GET TOKEN FROM AUTH HEADER
+	let jwtData = headers["Authorization"];
+
+	// CHECK IF TOKEN IS WITH BEARER PREFIX AND REMOVE IT
+	const splitAuthHeader = jwtData.split(" ");
+	if (splitAuthHeader.length == 2 && splitAuthHeader[0] == "Bearer")
+		jwtData = splitAuthHeader[1];
+
+	const jwtSplit = jwtData.split(".");
+	try {
+		return { ...JSON.parse(atob(jwtSplit[1])), jwt: jwtData };
+	} catch (error) {
+		throw new Error("Invalid token");
+	}
 }

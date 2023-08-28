3017495aefc55995a539a3cffe68690c3a6f8b03
Nikxy <nikxy3@gmail.com>
2023-08-28 16:37:07 +0300

    create findUser & createSession

--- src/utils/authRepository.js
+++ src/utils/authRepository.js
@@ -3,6 +3,8 @@ import {
 	DynamoDBDocumentClient,
 	GetCommand,
 	UpdateCommand,
+	ScanCommand,
+	PutCommand,
 } from "@aws-sdk/lib-dynamodb";
 
 import { v4 as uuidv4 } from "uuid";
@@ -48,6 +50,54 @@ class Repository {
 		);
 		return refreshToken;
 	};
+
+	findUser = async (domain, username) => {
+		const results = await this.docClient.send(new ScanCommand({
+			TableName: process.env.DB_TABLE,
+
+			ExpressionAttributeValues: {
+				":typeVal": "user",
+				":name": username,
+				":domainVal": domain,
+			},
+			FilterExpression:
+				"doc_type = :typeVal and username = :name and doc_domain = :domainVal",
+		}));
+
+		if (results.Items.length < 1)
+			return null
+
+		return results.Items[0];
+	};
+	createSession = async (domain, userid,user_ip,user_agent) => {
+		const sessionID = uuidv4();
+		const refreshToken = uuidv4();
+		const dateNow = Date.now();
+
+		try {
+			await this.docClient.send(new PutCommand({
+				TableName: process.env.DB_TABLE,
+				Item: {
+					id: sessionID,
+					doc_type: "session",
+					doc_domain: domain,
+					refresh_token: refreshToken,
+					userid: userid,
+					created: dateNow,
+					expires: dateNow + 604800, // 7 days
+					last_used: dateNow,
+					user_ip: user_ip,
+					user_agent: user_agent,
+				},
+			}));
+		} catch (e) {
+			console.error("DB: " + e.message);
+			return response.ServerError();
+		}
+
+
+		return [sessionID, refreshToken];
+	}
 }
 
 export default new Repository();

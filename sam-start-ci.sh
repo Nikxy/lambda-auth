ed1bb62a6ce7e5ccf9bfcf24d117a31e6b7dbc97
Nikxy <nikxy3@gmail.com>
2023-09-04 17:09:42 +0300

    remove sam ci testing to use cloudformation

--- sam-start-ci.sh
+++
@@ -1,5 +0,0 @@
-venv/bin/sam local start-api -t test-template.yaml \
---docker-network ssl-proxy \
---container-host 172.17.0.1 \
---container-host-interface 0.0.0.0 \
--v /home/diana/dev/jenkins_workspace/auth.nikxy.dev
--- sam-start.sh
+++ sam-start.sh
@@ -1,4 +1,4 @@
-sam local start-api -t test-template.yaml \
+sam local start-api -t sam-test-template.yaml \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \
 --container-host-interface 0.0.0.0 \

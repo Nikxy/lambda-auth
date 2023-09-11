0fac4ef74ccfb55e35e4bb304c8e1e34449ebd5a
Nikxy <nikxy3@gmail.com>
2023-09-11 12:19:57 +0300

    add warm containers

--- sam-start.sh
+++ sam-start.sh
@@ -4,4 +4,5 @@ sam local start-api -t cloudformation.yml \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \
 --container-host-interface 0.0.0.0 \
---host 0.0.0.0 $@
+--host 0.0.0.0 \
+--warm-containers EAGER $@

30c66440c3ae89d4950ecef722f478e0cdf39c2c
Nikxy <nikxy3@gmail.com>
2023-09-11 16:08:13 +0300

    use warm containers

--- sam-start-ci.sh
+++ sam-start-ci.sh
@@ -4,4 +4,5 @@ venv/bin/sam local start-api \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \
 --container-host-interface 0.0.0.0 \
+--warm-containers EAGER \
 -v /home/diana/dev/jenkins_workspace/auth.nikxy.dev

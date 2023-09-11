2d6ae830254eabebf35a23a3dc61b675ddd7a637
Nikxy <nikxy3@gmail.com>
2023-09-11 10:29:50 +0300

    use new cloudformation template for testing

---
+++ sam-start-ci.sh
@@ -0,0 +1,6 @@
+venv/bin/sam local start-api -t cloudformation.yml \
+--parameter-overrides ParameterKey=EnvironmentType,ParameterValue=test \
+--docker-network ssl-proxy \
+--container-host 172.17.0.1 \
+--container-host-interface 0.0.0.0 \
+-v /home/diana/dev/jenkins_workspace/auth.nikxy.dev

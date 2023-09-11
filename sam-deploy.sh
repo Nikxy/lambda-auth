2417b1f95879c9950b2d0f672d021275bedc566d
Nikxy <nikxy3@gmail.com>
2023-09-11 18:08:24 +0300

    remove manual deploy script

--- sam-deploy.sh
+++
@@ -1,7 +0,0 @@
-sam deploy \
---stack-name nikxy-auth \
---region il-central-1 \
---s3-bucket nikxy-cloudformation \
---s3-prefix sam-nikxy-auth \
---on-failure ROLLBACK \
---capabilities CAPABILITY_NAMED_IAM

9ee5731b9ed7c9a939f8d055337cf57aea132790
Nikxy <nikxy3@gmail.com>
2023-09-11 12:09:33 +0300

    add region setting

--- sam-start.sh
+++ sam-start.sh
@@ -1,4 +1,5 @@
 sam local start-api -t cloudformation.yml \
+--region il-central-1 \
 --parameter-overrides ParameterKey=EnvironmentType,ParameterValue=test \
 --docker-network ssl-proxy \
 --container-host 172.17.0.1 \

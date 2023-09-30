57dbcace67ee79901ec473b45526f18fd26bc363
Nikxy <nikxy3@gmail.com>
2023-09-30 18:09:45 +0300

    fix local endpoint false

--- src/utils/initConfig.js
+++ src/utils/initConfig.js
@@ -3,7 +3,7 @@ export default function () {
 	const config = { region: process.env.REGION };
 
     // Check if local endpoint provided and apply it
-	if (process.env.LOCAL_ENDPOINT && process.env.LOCAL_ENDPOINT != false) {
+	if (process.env.LOCAL_ENDPOINT && process.env.LOCAL_ENDPOINT != "false") {
 		console.log('Local endpoint provided, using it instead of AWS');
 		config.endpoint = process.env.LOCAL_ENDPOINT;
 		config.credentials = {

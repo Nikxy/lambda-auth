30ee9b0117f496ee25cf482293dea7ffd74fcd50
Nikxy <nikxy3@gmail.com>
2023-10-04 16:17:28 +0300

    remove secretmanager time test

--- src/utils/getSecret.js
+++ src/utils/getSecret.js
@@ -7,28 +7,20 @@ import initAWSConfig from "#utils/initConfig.js";
 
 const clientSecrets = new SecretsManagerClient(initAWSConfig());
 
-let secretString = null;
 export default async function (secret_name) {
 	let response;
-
-	//if (secretString == null) {
-		console.time("secretsmanager");
-		try {
-			response = await clientSecrets.send(
-				new GetSecretValueCommand({ SecretId: secret_name })
-			);
-		} catch (error) {
-			// For a list of exceptions thrown, see
-			// https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-			throw error;
-		}
-		console.timeEnd("secretsmanager");
-		secretString = response.SecretString;
-	/*} else {
-		console.log("Using cached secret");
-	}*/
+	
 	try {
-		return JSON.parse(secretString);
+		response = await clientSecrets.send(
+			new GetSecretValueCommand({ SecretId: secret_name })
+		);
+	} catch (error) {
+		// For a list of exceptions thrown, see
+		// https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+		throw error;
+	}
+	try {
+		return JSON.parse(response.SecretString);
 	} catch (e) {
 		throw e;
 	}

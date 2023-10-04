d71d6bb041868f70475a8b6ee6f70cd2f5b436e9
Nikxy <nikxy3@gmail.com>
2023-10-04 15:39:05 +0300

    add secrets manager time test

--- src/utils/getSecret.js
+++ src/utils/getSecret.js
@@ -1,27 +1,35 @@
 import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
+	SecretsManagerClient,
+	GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
 
 import initAWSConfig from "#utils/initConfig.js";
 
 const clientSecrets = new SecretsManagerClient(initAWSConfig());
 
-export default async function(secret_name) {
-  let response;
+let secretString = null;
+export default async function (secret_name) {
+	let response;
 
-  try {
-    response = await clientSecrets.send(
-      new GetSecretValueCommand({ SecretId: secret_name })
-    );
-  } catch (error) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    throw error;
-  }
-  try {
-    return JSON.parse(response.SecretString);
-  } catch (e) {
-    throw e;
-  }
+	//if (secretString == null) {
+		console.time("secretsmanager");
+		try {
+			response = await clientSecrets.send(
+				new GetSecretValueCommand({ SecretId: secret_name })
+			);
+		} catch (error) {
+			// For a list of exceptions thrown, see
+			// https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+			throw error;
+		}
+		console.timeEnd("secretsmanager");
+		secretString = response.SecretString;
+	/*} else {
+		console.log("Using cached secret");
+	}*/
+	try {
+		return JSON.parse(secretString);
+	} catch (e) {
+		throw e;
+	}
 }
